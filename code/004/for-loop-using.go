e08db564ec750d9407e29af336527f3ebeb6dfa5
wumoxi <wu.shaohua@foxmail.com>
2019-10-25 23:51:33 +0800

    添加【流程控制】代码片段

--- code/004/for-loop-using.go
+++ code/004/for-loop-using.go
@@ -6,7 +6,7 @@ func main() {
 	level := 9
 	for i := 1; i <= level; i++ {
 		for j := 1; j <= i; j++ {
-			fmt.Printf("%d * %d = %d\t", i, j, i*j)
+			fmt.Printf("%d * %d = %d\t", j, i, i*j)
 		}
 		fmt.Println()
 	}
