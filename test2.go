9ae77174d3ee885e7ec814cd3513b79d37538951
wumoxi <wu.shaohua@foxmail.com>
2019-11-05 11:15:21 +0800

    添加【go语言结构体】说明

--- test2.go
+++ test2.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+)
+
 func main() {
-	// 声明
+	wr := bytes.NewBuffer(nil)
+	w := bufio.NewWriter(wr)
+	data := []byte("Hello World")
+	w.Write(data)
+	fmt.Printf("未执行 Flush 缓冲区输出 %q\n")
 }
