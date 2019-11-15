439ffe9b56240518c5bb4461033aa4cc4f1653c8
wumoxi <wu.shaohua@foxmail.com>
2019-11-15 15:32:27 +0800

    添加【go语言反射】代码片段

--- code/010/reflect-get-struct-members-value/reflect-get-struct-members-value.go
+++ code/010/reflect-get-struct-members-value/reflect-get-struct-members-value.go
@@ -34,7 +34,6 @@ func main() {
 		// 试图修改 Name 字段值
 		if fieldName == "Name" {
 			v.FieldByName(fieldName).SetString("武沫汐")
-			fmt.Println("v.FieldByName(fieldName):", v.FieldByName(fieldName))
 		}
 
 		// 字段类型
